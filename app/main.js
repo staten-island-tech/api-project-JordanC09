f6f2f5ce0dae395a1c80388a2855f9b7d7ff406c
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-11-20 14:02:40 -0500

    i like the elvis bells

--- app/main.js
+++ app/main.js
@@ -1,2 +1,22 @@
 import "./style.css";
-//JUST GIVE ME MY MONEY
+//JUST GIVE ME MY API
+async function getData() {
+  try {
+    // returns a promise
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      const data = await response.json();
+      console.log(response);
+      //this is unique to THIS API!!!!!!!!!!
+      data.forEach((country) => console.log(country.name));
+    }
+  } catch (error) {
+    alert("hey I could not find that agent unc");
+  }
+}
+
+getData();
