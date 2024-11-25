7d013ca6784650abcb3cd2a1ca35b4a66bef51f0
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-11-25 14:00:17 -0500

    aefs

--- app/main.js
+++ app/main.js
@@ -1,6 +1,11 @@
 import "./style.css";
 //JUST GIVE ME MY API
 
+const DOMSelectors = {
+  start: document.querySelector(".btn btn-outline btn-info"),
+};
+
+let data = [];
 async function getData() {
   try {
     // returns a promise
@@ -10,7 +15,7 @@ async function getData() {
       throw new Error(response);
     } else {
       //convert promise to json
-      let data = await response.json();
+      data = await response.json();
       console.log(data);
       //this is unique to THIS API!!!!!!!!!!
       data.forEach((country) => console.log(country.name));
@@ -21,3 +26,33 @@ async function getData() {
 }
 
 getData();
+async function bruh() {
+  try {
+    // returns a promise
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      data = await response.json();
+      console.log(data);
+      //this is unique to THIS API!!!!!!!!!!
+      data.forEach((country) => {
+        if (country.name.common === "Grenada") {
+          console.log("uygfdjhsad");
+        }
+      });
+    }
+  } catch (error) {
+    alert("hey I could not find that agent unc");
+  }
+}
+bruh();
+// data.forEach((country) => console.log(country.name));
+
+// data.forEach((country) => {
+//   if (country.name === "Grenada") {
+//     console.log("jhgadjhfgdsjhgf");
+//   }
+// });
